41f3ceac211a0fb3779cdae05b696c0b04ef6985
MoreAndMore <abulljk@gmail.com>
2025-09-14 01:44:18 +0500

    bare minimum frontend

---
+++ ocr-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DocumentLab from './pages/DocumentLab.jsx'
+
+export default function App() {
+  return (
+    <div style={{ padding: 24, fontFamily: 'system-ui, sans-serif' }}>
+      <h1>It works 🎉</h1>
+      <p>If you see this, React mounted correctly.</p>
+      <DocumentLab />
+    </div>
+  )
+}
