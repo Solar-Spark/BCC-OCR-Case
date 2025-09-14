e49b2fbe77c04fee6a8e5f03e3fedbe2256bb7b8
perpavbek <pasha_perec@hotmail.com>
2025-09-14 18:56:33 +0500

    feat: proccess json

--- ocr-backend/main.py
+++ ocr-backend/main.py
@@ -1,11 +1,20 @@
+import sys
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse
 from pathlib import Path
+import asyncio
 import shutil
 import mimetypes
-
+from ocr import ocr_images
+from doc_preprocess import preprocess_document
+from llm.llm_pipeline import extract_contract_json
 app = FastAPI()
 
 # --- CORS (allow your Vite dev server) ---
@@ -40,20 +49,15 @@ async def upload(file: UploadFile = File(...)):
     cleaned_path = CLEANED_DIR / cleaned_name
     shutil.copy(upload_path, cleaned_path)
 
+    contract_json = extract_contract_json(ocr_images(preprocess_document(cleaned_path)))
+
     mime, _ = mimetypes.guess_type(cleaned_path.name)
 
     # Send back the cleaned file URL and metadata
     return JSONResponse({
         "cleaned_url": f"/cleaned/{cleaned_name}",  # Make sure this path works in the frontend
         "mime": mime or "application/pdf",
-        "fields": {
-            "document_type": "invoice",
-            "invoice_number": "INV-1024",
-            "date": "2025-09-14",
-            "total_amount": 12345.67,
-            "currency": "KZT",
-            "vendor": "ACME Bank"
-        },
+        "fields": contract_json,
         "metrics": {
             "cer": 0.121,
             "wer": 0.198,
